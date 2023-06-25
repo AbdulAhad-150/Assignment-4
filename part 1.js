35d52283eb58a40cc93bcd11629e794496ec43c0
AbdulAhad-150 <abdulahad150@gmail.com>
2023-06-25 05:58:10 -0700

    java work

---
+++ part 1.js
@@ -0,0 +1,20 @@
+//QUESTION 1 and 2
+alert ("Error!Please enter a valid password") ;
+
+//question 3
+alert("Welcome to JS Land\nHappy Coding!" )
+
+//QUESTION 4
+alert ("Welcome to js Land")
+
+
+//QUESTION 5
+alert("HELLO...I can run JS through my web browser`s console")
+
+//QUESTION 7
+//7. Practice placement of <script></script> element in
+//following sections of your project in exercise 6:
+
+//C is right anwer
+
+
---
+++ part 2.js
@@ -0,0 +1,3 @@
+
+var myname ="ahad";
+console.log(myname)
